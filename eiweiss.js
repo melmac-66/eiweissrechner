fe378487e179d7cdf96c7a4d00e9fbdc2020fd2f
gf <gf@nezzwerk.de>
2018-06-10 14:51:16 +0200

    gf

--- eiweiss.js
+++ eiweiss.js
@@ -1,6 +1,27 @@
 $().ready(function(){
-  alert("testalarm");
-  $("h1").click(function(){
-    alert("hallo Thora!");
+
+  $.ajax({
+    url:"lebensmittel.csv",
+    contentType: "text/csv"
+  }).done(function(data) {
+    lines=data.split("\n");
+    for (line in lines) {
+      if (line>0) {
+        parts=lines[line].split("\t");
+        $("#lebensmittel").append( '<option value="'+parts[6]+'">'+parts[0]+'</option>');
+      }
+    }
+  })
+
+/*  $('#lebensmittel').change(function() {
+    alert($(this).val()+"g eiweiss pro 100 g");
+  })
+*/
+  $("#berechnen").submit(function(event){
+    event.preventDefault();
+    lebenmittelwert=parseFloat($('#lebensmittel').val().replace(/,/,'.'));
+    faktor=parseFloat($('#menge').val());
+    alert("hallo Thora, du hast "+(lebenmittelwert/100*faktor)+" g Eiweiß gegessen");
+    return false;
   })
 })
